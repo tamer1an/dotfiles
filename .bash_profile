7c383165f25e47fa5b18be1dd50e22d9f9fd8550
Andrii <tamerjlah@gmail.com>
2020-05-16 22:26:14 +0200

    Initial commit template from Paul Irish

---
+++ .bash_profile
@@ -0,0 +1,47 @@
+# Load our dotfiles like ~/.bash_prompt, etc…
+#   ~/.extra can be used for settings you don’t want to commit,
+#   Use it to configure your PATH, thus it being first in line.
+for file in ~/.{extra,bash_prompt,exports,aliases,functions}; do
+    [ -r "$file" ] && source "$file"
+done
+unset file
+
+# highlighting inside manpages and elsewhere
+export LESS_TERMCAP_mb=$'\E[01;31m'       # begin blinking
+export LESS_TERMCAP_md=$'\E[01;38;5;74m'  # begin bold
+export LESS_TERMCAP_me=$'\E[0m'           # end mode
+export LESS_TERMCAP_se=$'\E[0m'           # end standout-mode
+export LESS_TERMCAP_so=$'\E[38;5;246m'    # begin standout-mode - info box
+export LESS_TERMCAP_ue=$'\E[0m'           # end underline
+export LESS_TERMCAP_us=$'\E[04;38;5;146m' # begin underline
+
+# Enable history expansion with space
+# E.g. typing !!<space> will replace the !! with your last command
+bind Space:magic-space
+
+# Use standard ISO 8601 timestamp
+# %F equivalent to %Y-%m-%d
+# %T equivalent to %H:%M:%S (24-hours format)
+export HISTTIMEFORMAT='%F %T '
+
+# keep history up to date, across sessions, in realtime
+#  http://unix.stackexchange.com/a/48113
+export HISTCONTROL="ignoredups"       # no duplicate entries, but keep space-prefixed commands
+export HISTSIZE=100000                          # big big history (default is 500)
+export HISTFILESIZE=$HISTSIZE                   # big big history
+type shopt &> /dev/null && shopt -s histappend  # append to history, don't overwrite it
+
+# Don't record some commands
+export HISTIGNORE="&:[ ]*:exit:ls:bg:fg:history:clear"
+
+# Save multi-line commands as one command
+shopt -s cmdhist
+
+export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# Enable git branch name completion if file exists
+if [ -f ~/.git-completion.bash ]; then
+  . ~/.git-completion.bash
+fi
---
+++ setup-a-new-machine.sh
@@ -0,0 +1,75 @@
+# copy paste this file in bit by bit.
+# don't run it.
+  echo "do not run this script in one go. hit ctrl-c NOW"
+  read -n 1
+
+##############################################################################################################
+###  backup old machine's key items
+
+mkdir -p ~/migration/home/
+mkdir -p ~/migration/Library/"Application Support"/
+mkdir -p ~/migration/Library/Preferences/
+mkdir -p ~/migration/Library/Application Support/
+mkdir -p ~/migration/rootLibrary/Preferences/SystemConfiguration/
+
+cd ~/migration
+
+# what is worth reinstalling?
+npm list -g --depth=0    > npm-g-list.txt
+yarn global list --depth=0 > yarn-g-list.txt
+
+# backup some dotfiles likely not under source control
+cp -Rp \
+    ~/.bash_history \
+    ~/.extra ~/.extra.fish \
+    ~/.gitconfig.local \
+    ~/.gnupg \
+    ~/.nano \
+    ~/.nanorc \
+    ~/.netrc \
+    ~/.ssh \
+    ~/.z   \
+        ~/migration/home
+
+cp -Rp ~/Documents ~/migration
+
+cp -Rp /Library/Preferences/SystemConfiguration/com.apple.airport.preferences.plist ~/migration/rootLibrary/Preferences/SystemConfiguration/ # wifi
+
+cp -Rp ~/Library/Preferences/net.limechat.LimeChat.plist ~/migration/Library/Preferences/
+cp -Rp ~/Library/Preferences/com.tinyspeck.slackmacgap.plist ~/migration/Library/Preferences/
+
+cp -Rp ~/Library/Services ~/migration/Library/ # automator stuff
+cp -Rp ~/Library/Fonts ~/migration/Library/ # all those fonts you've installed
+
+# editor settings & plugins
+cp -Rp ~/Library/Application\ Support/Sublime\ Text\ * ~/migration/Library/"Application Support"
+cp -Rp ~/Library/Application\ Support/Code\ -\ Insider* ~/migration/Library/"Application Support"
+
+##############################################################################################################
+### install of common things
+###
+
+# autocompletion for git branch names https://git-scm.com/book/en/v1/Git-Basics-Tips-and-Tricks
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
+
+# Type `git open` to open the GitHub page or website for a repository.
+npm install -g git-open
+
+# fancy listing of recent branches
+npm install -g git-recent
+
+# nice git diffs
+npm install -g diff-so-fancy
+
+# trash as the safe `rm` alternative
+#npm install --global trash-cli
+
+
+# faster git server communication.
+# like a LOT faster. https://opensource.googleblog.com/2018/05/introducing-git-protocol-version-2.html
+#git config protocol.version 2
+
+# also this unrelated thing
+# git config user.email "xxxx@chromium.org"
+
+./symlink-setup.sh
